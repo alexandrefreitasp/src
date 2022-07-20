f90351c6c5ee84d74f4b31f0c4c44ec1325bae9c
alexandrefreitasp <alexandrefreitasp@gmail.com>
2022-07-20 17:31:44 -0300

    Commit Contas

---
+++ banco/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"banco/contas"
+	"fmt"
+)
+
+func main() {
+
+	contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
+	contaDoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 100}
+
+	status := contaDoGustavo.Tranferir(-200, &contaDaSilvia)
+
+	fmt.Println(status)
+	fmt.Println(contaDaSilvia)
+	fmt.Println(contaDoGustavo)
+
+}
