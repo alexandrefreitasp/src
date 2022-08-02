1ccd7f674ea5902eb64686d67702e6cbe8ba6e24
alexandrefreitasp <alexandrefreitasp@gmail.com>
2022-08-02 14:16:33 -0300

    Gorilla mux

--- api-go-rest/main.go
+++ api-go-rest/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"api-go-rest/models"
+	"api-go-rest/routes"
 	"fmt"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
+		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
+	}
 	fmt.Println("Iniciando p servidor Rest com Go")
-	HandleRequest()
+	routes.HandleRequest()
 }
