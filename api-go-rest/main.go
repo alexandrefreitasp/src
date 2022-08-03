21eb1abadef9d3c0f16b7dd286b8952c05d7b633
alexandrefreitasp <alexandrefreitasp@gmail.com>
2022-08-03 15:20:56 -0300

    Final

--- api-go-rest/main.go
+++ api-go-rest/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"api-go-rest/models"
+	"api-go-rest/database"
 	"api-go-rest/routes"
 	"fmt"
 )
 
 func main() {
-	models.Personalidades = []models.Personalidade{
-		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
-		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
-	}
-	fmt.Println("Iniciando p servidor Rest com Go")
+
+	database.ConectaComBancoDeDados()
+	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
